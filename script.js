5c06436528ae9027fb0d8a1314965e0214917f67
Vadim Faust <vadim40328@gmail.com>
2024-08-20 02:23:48 +0700

    WIP Participant slider

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function ready(fn) {
 }
 
 const init_participants = () => {
-  const get_total_pages = () => {
+  const get_per_page_amount = () => {
     const page_width = window.innerWidth;
     
+    // These values must match the CSS rules
     // TODO(vf): set propper page width values
     if (page_width >= 1200) {
       return 3;
@@ -20,11 +21,28 @@ const init_participants = () => {
     }
   }
 
+
   document.querySelectorAll(".participants").forEach((el) => {
     const slider_el = el.querySelector(".slider")
     const el_count = slider_el.childElementCount;
-    let page_count = Math.ceil(el_count / get_total_pages());
+    let per_page_amount = get_per_page_amount();
+    let page_count = Math.ceil(el_count / per_page_amount);
     el.querySelector(".counter .total").textContent = page_count;
+
+    addEventListener("resize", () => {
+      const per_page_updated = get_per_page_amount();
+      if (per_page_amount != per_page_updated) {
+        per_page_amount = per_page_updated;
+        page_count = Math.ceil(el_count / per_page_amount);
+        el.querySelector(".counter .total").textContent = page_count;
+        // Go to first page if current page is more that page count after resize
+        if (parseInt(slider_el.dataset.current) > page_count) {
+          slider_el.dataset.current = 1;
+          slider_el.style.transform = `none`;
+          el.querySelector(".counter .current").textContent = 1;
+        }
+      }
+    });
 
     const update_slider_pos = (current_page) => {
       slider_el.dataset.current = current_page;
